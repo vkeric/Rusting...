9ae2a49410092e3efe4122f988c01aed2a6205a3
鸵鸟 <2950995347@qq.com>
2024-09-26 23:44:44 +0800

    cargo run~

---
+++ deck/target/debug/deck.d
@@ -0,0 +1,1 @@
+G:\_Rust\deck\target\debug\deck.exe: G:\_Rust\deck\src\main.rs
---
+++ deck/target/debug/deps/deck.d
@@ -0,0 +1,5 @@
+G:\_Rust\deck\target\debug\deps\deck.exe: src\main.rs
+
+G:\_Rust\deck\target\debug\deps\deck.d: src\main.rs
+
+src\main.rs:
