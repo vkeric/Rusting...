30fbdc66b0c43b5d8fbf4eb93944a7b15fc311d4
鸵鸟 <2950995347@qq.com>
2024-09-27 23:06:39 +0800

    strcut || Vec

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,3 +1,19 @@
+// 诡异的概念...
+// 复杂的struct 和 复杂的向量
+struct Deck{
+    cards: Vec<String>,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let suits = ["Vk","is","Nb","!!"];
+    let values = ["1","2","3","4"];
+    // 绑定 _ 结构体实例
+    let deck = Deck{ cards : vec![
+
+    ]};
+
+    // println!("Hello, world!");
+    println!("Heres yours deck : {}", deck );
 }
